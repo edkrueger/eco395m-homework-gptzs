b5785fdb651698d4f86719efc0f4362c8d826ebf
Edward Krueger <edkrueger@gmail.com>
2024-10-22 12:17:42 -0500

    Update classify.py to new api

--- code/classify.py
+++ code/classify.py
@@ -1,4 +1,5 @@
 import pandas as pd
+from openai import OpenAI
 from prompt_classifier import classify_text_message
 from sklearn.metrics import classification_report
 from tqdm import tqdm
@@ -13,16 +14,17 @@ def load_data(path):
     """
     # write this
 
-def classify_all(df, api_key):
+
+def classify_all(df, client):
     """Takes a DataFrame with series named "label" and "text".
     Returns a pd.DataFrame with series named "label" and "text"
     and also "predicted_label" which is our zero-shot classifier's prediction.
     """
 
-    # write this
-    # in order to be able to use a progress bar with epd.DataFram
+    # in order to be able to use a progress bar with pd.DataFrame
     # tqdm can modify pd.DataFrame's methods to include `progress_apply`
     # which works like apply, but adds a progress bar
+    # write this
 
 
 if __name__ == "__main__":
@@ -34,6 +36,8 @@ if __name__ == "__main__":
     dotenv.load_dotenv()
 
     OPENAI_API_KEY = os.environ["OPENAI_API_KEY"]
+    client = OpenAI(api_key=OPENAI_API_KEY, max_retries=0)
+
 
     IN_PATH = sys.argv[1]
     OUT_PATH = sys.argv[2]
@@ -43,7 +47,7 @@ if __name__ == "__main__":
 
     in_df = load_data(IN_PATH)
 
-    out_df = classify_all(in_df, api_key=OPENAI_API_KEY)
+    out_df = classify_all(in_df, client=client)
 
     out_df.to_csv(OUT_PATH, index=False)
 
